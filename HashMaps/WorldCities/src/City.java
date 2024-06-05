c85edc203260aa67a906f9d4146b3cd3058ff421
Felix Mairamhof <feberg.k@gmail.com>
2024-06-05 09:33:21 +0200

    Build city class

---
+++ HashMaps/WorldCities/src/City.java
@@ -0,0 +1,56 @@
+public class City
+{
+    private String name;
+    private int population;
+    private String country;
+    private String iso3;
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", population=" + population +
+                ", country='" + country + '\'' +
+                ", iso3='" + iso3 + '\'' +
+                '}';
+    }
+
+    public City(String name, int population, String country, String iso3) {
+        this.name = name;
+        this.population = population;
+        this.country = country;
+        this.iso3 = iso3;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIso3() {
+        return iso3;
+    }
+
+    public void setIso3(String iso3) {
+        this.iso3 = iso3;
+    }
+}
